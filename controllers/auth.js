630ce0a760902bea7b8303c409d355ebf4adebc4
RawanAli <rawan.ali.fardan@email.com>
2025-09-10 16:32:39 +0300

    sign-out, vip added

---
+++ controllers/auth.js
@@ -0,0 +1,69 @@
+const bcrypt = require("bcrypt");
+const User = require('../models/user.js');
+const express = require("express");
+const router = express.Router();
+
+// requiring the past users registered
+// passwords must be secured 
+
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
+
+
+
+router.post('/sign-up', async (req, res) => {
+
+    const userInDatabase = await User.findOne({username: req.body.username});
+
+    if(userInDatabase) {
+    return res.send("username already taken");
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+  return res.send("Password and Confirm Password must match");
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+
+    const newuser = await User.create(req.body);
+    res.send(`Thanks for signing up ${newuser.username}`);
+    console.log(newuser)
+
+});
+
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
+
+
+router.post("/sign-in", async (req, res) => {
+const userInDatabase = await User.findOne({username: req.body.username});
+
+if(!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+}
+
+const validPassword = bcrypt.compareSync(
+  req.body.password,
+  userInDatabase.password
+);
+if (!validPassword) {
+  return res.send("Login failed. Please try again.");
+}
+
+req.session.user = {
+  username: userInDatabase.username,
+};
+
+res.redirect("/");
+
+});
+
+router.get("/sign-out", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
+module.exports = router;
